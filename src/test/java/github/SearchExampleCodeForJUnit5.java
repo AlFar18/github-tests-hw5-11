86f02cef27de000d6267a3d924c8be836e6f9e0e
AlFar18 <albina_fartdinova@mail.ru>
2022-03-05 15:17:08 +0300

    add Search Selenide

--- src/test/java/github/SearchExampleCodeForJUnit5.java
+++ src/test/java/github/SearchExampleCodeForJUnit5.java
@@ -5,18 +5,24 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SearchSelenideRepo {
+public class SearchExampleCodeForJUnit5 {
 
   @Test
-  void shouldFindSelenideInGithub() {
+  void shouldFindExampleCodeForJUnit5InGithub() {
     // открыть страницу github.com
     open("https://github.com/");
     // ввести в поле поиска selenide и нажать Enter
     $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
     // нажимаем на линк от первого результата поиска
     $$("ul.repo-list li").first().$("a").click();
-    // check: в заголовке встречается selenide/selenide
-    $("h1").shouldHave(text("selenide / selenide"));
+    // переход в раздел Wiki
+    $("#wiki-tab").click();
+    // поиск SoftAssertions
+    $("#wiki-pages-filter").setValue("SoftAssertions");
+    // клик по SoftAssertions
+    $$(".filterable-active a").findBy(text("SoftAssertions")).click();
+    // поиск кода для JUnit5
+    $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
   }
 }
 

1bbd3e4f94df9e54e8b970687dc4eeea0a39b071
AlFar18 <albina_fartdinova@mail.ru>
2022-04-26 22:43:48 +0300

    making adjustments

--- src/test/java/github/DragAndDropTest.java
+++ src/test/java/github/DragAndDropTest.java
@@ -1,8 +1,8 @@
 package github;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDropTest {
